a049f1393e188d3133435e3ddf96e4665b2f0261
bbadjari <bbadjari@4712a850-5839-4776-b159-00b35f6ef5bd>
2010-04-23 04:28:20 +0000

    Initial release 1.0.0.0.

---
+++ Argument/SwitchCollection.cs
@@ -0,0 +1,367 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2010 Bernard Badjari
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CSharpCLI.Argument
+{
+	/// <summary>
+	/// Managed collection of Switch objects.
+	/// </summary>
+	public class SwitchCollection : ICollection<Switch>, IEnumerable<Switch>, IList<Switch>
+	{
+		/// <summary>
+		/// Indices to switches in collection, accessed by long name.
+		/// </summary>
+		Dictionary<string, int> m_indicesByLongName;
+
+		/// <summary>
+		/// Indices to switches in collection, accessed by name.
+		/// </summary>
+		Dictionary<string, int> m_indicesByName;
+
+		/// <summary>
+		/// Switches in collection.
+		/// </summary>
+		List<Switch> m_switches;
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public SwitchCollection()
+		{
+			m_indicesByLongName = new Dictionary<string, int>();
+			m_indicesByName = new Dictionary<string, int>();
+			m_switches = new List<Switch>();
+		}
+
+		////////////////////////////////////////////////////////////////////////
+		// Methods
+
+		/// <summary>
+		/// Add switch with given name.
+		/// </summary>
+		public void Add(string name)
+		{
+			Add(new Switch(name));
+		}
+
+		/// <summary>
+		/// Add switch with given name and description.
+		/// </summary>
+		public void Add(string name, string description)
+		{
+			Add(new Switch(name, description));
+		}
+
+		/// <summary>
+		/// Add switch with given name, long name and description.
+		/// </summary>
+		public void Add(string name, string longName, string description)
+		{
+			Add(new Switch(name, longName, description));
+		}
+
+		/// <summary>
+		/// Add switch with given name, long name, description and whether
+		/// it is required.
+		/// </summary>
+		public void Add(string name, string longName, string description,
+			bool isRequired)
+		{
+			Add(new Switch(name, longName, description, isRequired));
+		}
+
+		/// <summary>
+		/// Add switch with given name, long name, description, whether
+		/// it has arguments and whether it is required.
+		/// </summary>
+		public void Add(string name, string longName, string description,
+			bool hasArguments, bool isRequired)
+		{
+			Add(new Switch(name, longName, description, hasArguments,
+				isRequired));
+		}
+
+		/// <summary>
+		/// Add switch with given name, long name, description, number of
+		/// expected arguments and whether it is required.
+		/// </summary>
+		public void Add(string name, string longName, string description,
+			int numberArguments, bool isRequired)
+		{
+			Add(new Switch(name, longName, description, numberArguments,
+				isRequired));
+		}
+
+		/// <summary>
+		/// Add switch with given name, long name, description, whether
+		/// it has arguments, whether it is required and name of
+		/// expected arguments.
+		/// </summary>
+		public void Add(string name, string longName, string description,
+			bool hasArguments, bool isRequired, string argumentName)
+		{
+			Add(new Switch(name, longName, description, hasArguments,
+				isRequired, argumentName));
+		}
+
+		/// <summary>
+		/// Add switch with given name, long name, description, whether it is
+		/// required and name of expected argument.
+		/// </summary>
+		public void Add(string name, string longName, string description,
+			bool isRequired, string argumentName)
+		{
+			Add(new Switch(name, longName, description, isRequired,
+				argumentName));
+		}
+
+		/// <summary>
+		/// Add switch with given name, long name, description, whether it is
+		/// required and names of expected arguments.
+		/// </summary>
+		public void Add(string name, string longName, string description,
+			bool isRequired, string[] argumentNames)
+		{
+			Add(new Switch(name, longName, description, isRequired,
+				argumentNames));
+		}
+
+		/// <summary>
+		/// Determine if given switch already added.
+		/// </summary>
+		public bool HasSwitch(Switch switchObject)
+		{
+			bool hasSwitch = false;
+
+			if (switchObject != null)
+			{
+				hasSwitch = m_switches.Contains(switchObject) ||
+					m_indicesByName.ContainsKey(switchObject.Name);
+
+				if (switchObject.HasLongName)
+					hasSwitch |= m_indicesByLongName.ContainsKey(switchObject.LongName);
+			}
+
+			return hasSwitch;
+		}
+
+		/// <summary>
+		/// Determine if switch with given name already added.
+		/// </summary>
+		public bool HasSwitch(string name)
+		{
+			return m_indicesByName.ContainsKey(name) || m_indicesByLongName.ContainsKey(name);
+		}
+
+		////////////////////////////////////////////////////////////////////////
+		// Properties
+
+		/// <summary>
+		/// Get switch with given name from collection.
+		/// </summary>
+		public Switch this[string name]
+		{
+			get
+			{
+				if (HasSwitch(name))
+				{
+					int index;
+
+					if (m_indicesByName.ContainsKey(name))
+						index = m_indicesByName[name];
+					else
+						index = m_indicesByLongName[name];
+
+					return Switches[index];
+				}
+
+				return null;
+			}
+		}
+
+		/// <summary>
+		/// Get all switches in collection sorted by name.
+		/// </summary>
+		public IList<Switch> SortedSwitches
+		{
+			get
+			{
+				List<Switch> sortedSwitches = new List<Switch>(Switches);
+
+				sortedSwitches.Sort(new SwitchComparer());
+
+				return sortedSwitches.ToArray();
+			}
+		}
+
+		/// <summary>
+		/// Get all switches in collection.
+		/// </summary>
+		public IList<Switch> Switches
+		{
+			get { return m_switches; }
+		}
+
+		////////////////////////////////////////////////////////////////////////
+		// ICollection<T>
+
+		/// <summary>
+		/// Get number of switches in collection.
+		/// </summary>
+		public int Count
+		{
+			get { return m_switches.Count; }
+		}
+
+		/// <summary>
+		/// Determine if collection is read-only.
+		/// </summary>
+		public bool IsReadOnly
+		{
+			get { return false; }
+		}
+
+		/// <summary>
+		/// Add given switch to collection.
+		/// </summary>
+		public void Add(Switch item)
+		{
+			if (item == null)
+				return;
+
+			if (!HasSwitch(item))
+			{
+				int newIndex = Count;
+
+				m_switches.Add(item);
+
+				m_indicesByName.Add(item.Name, newIndex);
+
+				if (item.HasLongName)
+					m_indicesByLongName.Add(item.LongName, newIndex);
+			}
+		}
+
+		/// <summary>
+		/// Remove all switches from collection.
+		/// </summary>
+		public void Clear()
+		{
+			m_indicesByLongName.Clear();
+			m_indicesByName.Clear();
+			m_switches.Clear();
+		}
+
+		/// <summary>
+		/// Determine if given switch contained in collection.
+		/// </summary>
+		public bool Contains(Switch item)
+		{
+			return m_switches.Contains(item);
+		}
+
+		/// <summary>
+		/// Copy collection to given array, starting at given index.
+		/// </summary>
+		public void CopyTo(Switch[] array, int arrayIndex)
+		{
+			m_switches.CopyTo(array, arrayIndex);
+		}
+
+		/// <summary>
+		/// Remove first occurrence of given switch from collection.
+		/// 
+		/// Return whether switch successfully removed from collection.
+		/// </summary>
+		public bool Remove(Switch item)
+		{
+			return m_switches.Remove(item);
+		}
+
+		////////////////////////////////////////////////////////////////////////
+		// IEnumerable
+
+		/// <summary>
+		/// Get enumerator to use to iterate through collection.
+		/// </summary>
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return m_switches.GetEnumerator();
+		}
+
+		////////////////////////////////////////////////////////////////////////
+		// IEnumerable<T>
+
+		/// <summary>
+		/// Get enumerator to use to iterate through collection.
+		/// </summary>
+		public IEnumerator<Switch> GetEnumerator()
+		{
+			return m_switches.GetEnumerator();
+		}
+
+		////////////////////////////////////////////////////////////////////////
+		// IList<T>
+
+		/// <summary>
+		/// Get switch from collection at given index.
+		/// </summary>
+		public Switch this[int index]
+		{
+			get { return m_switches[index]; }
+			set { m_switches[index] = value; }
+		}
+
+		/// <summary>
+		/// Get index of given switch in collection.
+		/// </summary>
+		public int IndexOf(Switch item)
+		{
+			return m_switches.IndexOf(item);
+		}
+
+		/// <summary>
+		/// Insert given switch at given index in collection.
+		/// </summary>
+		public void Insert(int index, Switch item)
+		{
+			m_switches.Insert(index, item);
+		}
+
+		/// <summary>
+		/// Remove switch at given index in collection.
+		/// </summary>
+		public void RemoveAt(int index)
+		{
+			m_switches.RemoveAt(index);
+		}
+	}
+}
---
+++ Help/HelpPrinter.cs
@@ -0,0 +1,609 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2010 Bernard Badjari
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using CSharpCLI.Argument;
+
+namespace CSharpCLI.Help
+{
+	/// <summary>
+	/// Prints switch usage information and descriptions for a collection
+	/// of Switch objects.
+	/// </summary>
+	public class HelpPrinter
+	{
+		/// <summary>
+		/// Encloses argument names in output.
+		/// </summary>
+		static class ArgumentNameTag
+		{
+			public const string Start = "<";
+			public const string End = ">";
+		}
+
+		/// <summary>
+		/// Characters used in output.
+		/// </summary>
+		static class Character
+		{
+			/// <summary>
+			/// Length of one character.
+			/// </summary>
+			public const int Length = 1;
+
+			/// <summary>
+			/// Space character.
+			/// </summary>
+			public const char Space = ' ';
+
+			/// <summary>
+			/// Delimiter character.
+			/// </summary>
+			public const char Delimiter = ',';
+		}
+
+		/// <summary>
+		/// Encloses optional switches in output.
+		/// </summary>
+		static class OptionalSwitchTag
+		{
+			public const string Start = "[";
+			public const string End = "]";
+		}
+
+		/// <summary>
+		/// Number of space characters to use when printing descriptions.
+		/// </summary>
+		const int DescriptionSpacing = 3;
+
+		/// <summary>
+		/// No characters to indent each new line by.
+		/// </summary>
+		const int NoIndentCharacters = 0;
+
+		/// <summary>
+		/// Number of characters per line of output.
+		/// </summary>
+		const int OutputWidth = 80;
+
+		/// <summary>
+		/// Start of usage output.
+		/// </summary>
+		const string UsagePrefix = "Usage: ";
+
+		////////////////////////////////////////////////////////////////////////
+
+		/// <summary>
+		/// Executable name to be printed.
+		/// </summary>
+		string m_executableName;
+
+		/// <summary>
+		/// Footer to be printed.
+		/// </summary>
+		string m_footer;
+
+		/// <summary>
+		/// Header to be printed.
+		/// </summary>
+		string m_header;
+
+		/// <summary>
+		/// Number of characters to indent each new line by.
+		/// </summary>
+		int m_numberIndentCharacters;
+
+		/// <summary>
+		/// Help output to be printed.
+		/// </summary>
+		StringBuilder m_output;
+
+		/// <summary>
+		/// Switches to be printed.
+		/// </summary>
+		List<Switch> m_switches;
+
+		////////////////////////////////////////////////////////////////////////
+		// Constructors
+
+		/// <summary>
+		/// Constructor for specifying executable name and switches to be
+		/// printed.
+		/// </summary>
+		public HelpPrinter(string executableName, SwitchCollection switches)
+		{
+			m_executableName = executableName;
+			m_switches = new List<Switch>();
+
+			if (switches != null)
+				m_switches.AddRange(switches.SortedSwitches);
+		}
+
+		/// <summary>
+		/// Constructor for specifying header to be printed.
+		/// </summary>
+		public HelpPrinter(string executableName, SwitchCollection switches,
+			string header)
+			: this(executableName, switches)
+		{
+			m_header = header;
+		}
+
+		/// <summary>
+		/// Constructor for specifying footer to be printed.
+		/// </summary>
+		public HelpPrinter(string executableName, SwitchCollection switches,
+			string header, string footer)
+			: this(executableName, switches, header)
+		{
+			m_footer = footer;
+		}
+
+		////////////////////////////////////////////////////////////////////////
+		// Public Methods
+
+		/// <summary>
+		/// Print switch usage information and descriptions.
+		/// </summary>
+		public void Print()
+		{
+			Initialize();
+
+			PrintHeader();
+
+			if (HasSwitches)
+			{
+				PrintUsage();
+
+				AppendLines();
+
+				PrintDescriptions();
+
+				if (HasFooter)
+					AppendLines();
+			}
+
+			PrintFooter();
+
+			// Print help output to standard output.
+			Console.WriteLine(Output.ToString());
+		}
+
+		////////////////////////////////////////////////////////////////////////
+		// Methods
+
+		/// <summary>
+		/// Append given string value to output.
+		/// </summary>
+		void Append(string value)
+		{
+			string[] lines = Regex.Split(value, Environment.NewLine);
+
+			for (int index = 0; index < lines.Length; index++)
+			{
+				AppendLine(lines[index]);
+
+				if (index != lines.Length - 1)
+					AppendLine();
+			}
+		}
+
+		/// <summary>
+		/// Append given argument names to given output.
+		/// </summary>
+		static void AppendArgumentNames(string[] argumentNames, StringBuilder output)
+		{
+			for (int index = 0; index < argumentNames.Length; index++)
+			{
+				output.Append(ArgumentNameTag.Start);
+				output.Append(argumentNames[index]);
+				output.Append(ArgumentNameTag.End);
+
+				if (index != argumentNames.Length - 1)
+					output.Append(Character.Space);
+			}
+		}
+
+		/// <summary>
+		/// Append line and indentation to output.
+		/// </summary>
+		void AppendLine()
+		{
+			Output.AppendLine();
+			Output.Append(Indentation);
+		}
+
+		/// <summary>
+		/// Append given string value representing single line to output.
+		/// </summary>
+		void AppendLine(string value)
+		{
+			// Ensure given string value represents single line.
+			if (HasNewLine(value))
+			{
+				Append(value);
+
+				return;
+			}
+
+			// Maximum number of characters to append to line of output.
+			int maximumAppendLength = OutputWidth - Environment.NewLine.Length;
+
+			while (value.Length > 0)
+			{
+				int lineLengthLeft = maximumAppendLength - CurrentLineLength;
+
+				int appendLength = GetAppendLength(value, lineLengthLeft,
+					maximumAppendLength);
+
+				string outputLine = value.Substring(0, appendLength);
+
+				Output.Append(outputLine);
+
+				if (value.Length > lineLengthLeft)
+					AppendLine();
+
+				// Remove appended output, ensuring no leading whitespace.
+				value = value.Remove(0, appendLength).TrimStart();
+			}
+		}
+
+		/// <summary>
+		/// Append lines to output to separate sections.
+		/// </summary>
+		void AppendLines()
+		{
+			Output.AppendLine();
+			Output.AppendLine();
+		}
+
+		/// <summary>
+		/// Append given switch to given output.
+		/// </summary>
+		static void AppendSwitch(Switch switchObject, StringBuilder output)
+		{
+			if (switchObject.IsOptional)
+				output.Append(OptionalSwitchTag.Start);
+
+			output.Append(Switch.GetPrefixedName(switchObject.Name));
+
+			if (switchObject.HasArguments)
+			{
+				output.Append(Character.Space);
+
+				AppendArgumentNames(switchObject.GetArgumentNames(), output);
+			}
+
+			if (switchObject.IsOptional)
+				output.Append(OptionalSwitchTag.End);
+		}
+
+		/// <summary>
+		/// Determine if next token in given string starting at given index can
+		/// be appended on new line of output, given maximum amount.
+		/// </summary>
+		bool CanAppendNextToken(string value, int startIndex, int maximumAppendLength)
+		{
+			int firstSpaceIndex = value.IndexOf(Character.Space, startIndex);
+
+			int nextTokenLength = value.Length - startIndex;
+
+			if (firstSpaceIndex > 0)
+				nextTokenLength = firstSpaceIndex - startIndex;
+
+			return nextTokenLength + NumberIndentCharacters <= maximumAppendLength;
+		}
+
+		/// <summary>
+		/// Get number of characters in given string to append, depending on
+		/// whether it contains a new line character.
+		/// </summary>
+		static int GetAppendLength(string value)
+		{
+			int valueLength = value.Length;
+
+			int firstNewLineIndex = value.IndexOf(Environment.NewLine,
+				StringComparison.Ordinal);
+
+			if (firstNewLineIndex > 0)
+				valueLength = firstNewLineIndex;
+
+			return valueLength;
+		}
+
+		/// <summary>
+		/// Get number of characters in given string to append to current line
+		/// of output, given length left on current line and maximum length.
+		/// </summary>
+		int GetAppendLength(string value, int lineLengthLeft, int maximumAppendLength)
+		{
+			int appendLength = Math.Min(GetAppendLength(value), lineLengthLeft);
+
+			if (appendLength == lineLengthLeft)
+			{
+				string appendValue = value.Substring(0, appendLength);
+
+				int lastSpaceIndex = appendValue.LastIndexOf(Character.Space);
+
+				if (lastSpaceIndex > 0)
+				{
+					appendLength = lastSpaceIndex + Character.Length;
+
+					if (!CanAppendNextToken(value, appendLength, maximumAppendLength))
+						appendLength = lineLengthLeft;
+				}
+			}
+
+			return appendLength;
+		}
+
+		/// <summary>
+		/// Get padding of given length.
+		/// </summary>
+		static string GetPadding(int length)
+		{
+			return new String(Character.Space, length);
+		}
+
+		/// <summary>
+		/// Determine if given string value contains new line characters.
+		/// </summary>
+		static bool HasNewLine(string value)
+		{
+			return value.Contains(Environment.NewLine);
+		}
+
+		/// <summary>
+		/// Initialize printer.
+		/// </summary>
+		void Initialize()
+		{
+			m_output = new StringBuilder();
+
+			NumberIndentCharacters = NoIndentCharacters;
+		}
+
+		/// <summary>
+		/// Print switch descriptions.
+		/// </summary>
+		void PrintDescriptions()
+		{
+			List<string> namesArguments = new List<string>();
+
+			int longestLength = 0;
+
+			foreach (Switch switchObject in Switches)
+			{
+				StringBuilder nameArgument = new StringBuilder();
+
+				nameArgument.Append(Switch.GetPrefixedName(switchObject.Name));
+
+				if (switchObject.HasLongName)
+				{
+					nameArgument.Append(Character.Delimiter);
+					nameArgument.Append(Character.Space);
+					nameArgument.Append(Switch.GetLongPrefixedName(switchObject.LongName));
+				}
+
+				if (switchObject.HasArguments)
+				{
+					nameArgument.Append(Character.Space);
+
+					AppendArgumentNames(switchObject.GetArgumentNames(), nameArgument);
+				}
+
+				longestLength = Math.Max(longestLength, nameArgument.Length);
+
+				namesArguments.Add(nameArgument.ToString());
+			}
+
+			NumberIndentCharacters = longestLength + DescriptionSpacing;
+
+			for (int index = 0; index < Switches.Count; index++)
+			{
+				StringBuilder output = new StringBuilder();
+
+				Switch switchObject = Switches[index];
+
+				string nameArgument = namesArguments[index];
+
+				output.Append(nameArgument);
+
+				if (switchObject.HasDescription)
+				{
+					int paddingLength = NumberIndentCharacters - nameArgument.Length;
+
+					output.Append(GetPadding(paddingLength));
+					output.Append(switchObject.Description);
+				}
+
+				AppendLine(output.ToString());
+
+				if (index != Switches.Count - 1)
+					Output.AppendLine();
+			}
+		}
+
+		/// <summary>
+		/// Print footer.
+		/// </summary>
+		void PrintFooter()
+		{
+			NumberIndentCharacters = NoIndentCharacters;
+
+			if (HasFooter)
+				Append(Footer);
+		}
+
+		/// <summary>
+		/// Print header.
+		/// </summary>
+		void PrintHeader()
+		{
+			NumberIndentCharacters = NoIndentCharacters;
+
+			if (HasHeader)
+			{
+				Append(Header);
+
+				AppendLines();
+			}
+		}
+
+		/// <summary>
+		/// Print switch usage information.
+		/// </summary>
+		void PrintUsage()
+		{
+			NumberIndentCharacters = UsagePrefix.Length;
+
+			StringBuilder output = new StringBuilder();
+
+			output.Append(UsagePrefix);
+			output.Append(ExecutableName);
+			output.Append(Character.Space);
+
+			for (int index = 0; index < Switches.Count; index++)
+			{
+				AppendSwitch(Switches[index], output);
+
+				if (index != Switches.Count - 1)
+					output.Append(Character.Space);
+			}
+
+			AppendLine(output.ToString());
+		}
+
+		////////////////////////////////////////////////////////////////////////
+		// Properties
+
+		/// <summary>
+		/// Get length of current line of output.
+		/// </summary>
+		int CurrentLineLength
+		{
+			get
+			{
+				string output = Output.ToString();
+
+				int currentLineLength = output.Length;
+
+				int lastNewLineIndex = output.LastIndexOf(Environment.NewLine,
+					StringComparison.Ordinal);
+
+				if (lastNewLineIndex >= 0)
+					currentLineLength -= (lastNewLineIndex + Environment.NewLine.Length);
+
+				return currentLineLength;
+			}
+		}
+
+		/// <summary>
+		/// Get name of executable to be printed.
+		/// </summary>
+		string ExecutableName
+		{
+			get { return m_executableName; }
+		}
+
+		/// <summary>
+		/// Get footer to be printed.
+		/// </summary>
+		string Footer
+		{
+			get { return m_footer; }
+		}
+
+		/// <summary>
+		/// Determine if footer to be printed given.
+		/// </summary>
+		bool HasFooter
+		{
+			get { return !string.IsNullOrEmpty(Footer); }
+		}
+
+		/// <summary>
+		/// Determine if header to be printed given.
+		/// </summary>
+		bool HasHeader
+		{
+			get { return !string.IsNullOrEmpty(Header); }
+		}
+
+		/// <summary>
+		/// Determine if switches to be printed given.
+		/// </summary>
+		bool HasSwitches
+		{
+			get { return Switches.Count > 0; }
+		}
+
+		/// <summary>
+		/// Get header to be printed.
+		/// </summary>
+		string Header
+		{
+			get { return m_header; }
+		}
+
+		/// <summary>
+		/// Get indentation string to append to each new line of output.
+		/// </summary>
+		string Indentation
+		{
+			get { return GetPadding(NumberIndentCharacters); }
+		}
+
+		/// <summary>
+		/// Get/set number of characters to indent each new line by.
+		/// </summary>
+		int NumberIndentCharacters
+		{
+			get { return m_numberIndentCharacters; }
+			set { m_numberIndentCharacters = value; }
+		}
+
+		/// <summary>
+		/// Get help output to be printed.
+		/// </summary>
+		StringBuilder Output
+		{
+			get { return m_output; }
+		}
+
+		/// <summary>
+		/// Get switches to be printed.
+		/// </summary>
+		IList<Switch> Switches
+		{
+			get { return m_switches; }
+		}
+	}
+}
