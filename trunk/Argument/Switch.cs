c641f0433ef290b01387b8a0d114ad387d0feee7
bbadjari <bbadjari@4712a850-5839-4776-b159-00b35f6ef5bd>
2010-04-23 04:28:20 +0000

    Initial release 1.0.0.0.

---
+++ trunk/Argument/Switch.cs
@@ -0,0 +1,455 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2010 Bernard Badjari
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+
+namespace CSharpCLI.Argument
+{
+	/// <summary>
+	/// Pre-defined command-line argument.
+	/// </summary>
+	public class Switch
+	{
+		/// <summary>
+		/// Long prefix that identifies command-line argument as switch.
+		/// </summary>
+		public const string LongPrefix = "--";
+
+		/// <summary>
+		/// Prefix that identifies command-line argument as switch.
+		/// </summary>
+		public const string Prefix = "-";
+
+		////////////////////////////////////////////////////////////////////////
+
+		/// <summary>
+		/// No arguments expected to follow switch.
+		/// </summary>
+		const int NoArguments = 0;
+
+		/// <summary>
+		/// Switch not required (optional).
+		/// </summary>
+		const bool NotRequired = false;
+
+		/// <summary>
+		/// One argument expected to follow switch.
+		/// </summary>
+		const int OneArgument = 1;
+
+		/// <summary>
+		/// Prefixes that can identify command-line argument as switch.
+		/// </summary>
+		static readonly string[] Prefixes = new string[]
+		{
+			LongPrefix, Prefix
+		};
+
+		/// <summary>
+		/// Unknown number of arguments expected to follow switch.
+		/// </summary>
+		const int UnknownNumberArguments = int.MaxValue;
+
+		////////////////////////////////////////////////////////////////////////
+
+		/// <summary>
+		/// Names of arguments following switch.
+		/// </summary>
+		List<string> m_argumentNames;
+
+		/// <summary>
+		/// Values of arguments following switch.
+		/// </summary>
+		List<string> m_argumentValues;
+
+		/// <summary>
+		/// Description of switch.
+		/// </summary>
+		string m_description;
+
+		/// <summary>
+		/// True if arguments following switch expected, false otherwise.
+		/// </summary>
+		bool m_hasArguments;
+
+		/// <summary>
+		/// True if switch is required, false if optional.
+		/// </summary>
+		bool m_isRequired;
+
+		/// <summary>
+		/// Long name of switch.
+		/// </summary>
+		string m_longName;
+
+		/// <summary>
+		/// Name of switch.
+		/// </summary>
+		string m_name;
+
+		/// <summary>
+		/// Number of arguments expected to follow switch.
+		/// </summary>
+		int m_numberArguments;
+
+		////////////////////////////////////////////////////////////////////////
+		// Constructors
+
+		/// <summary>
+		/// Constructor for specifying switch name.
+		/// </summary>
+		public Switch(string name)
+			: this(name, null)
+		{
+		}
+
+		/// <summary>
+		/// Constructor for specifying switch description.
+		/// </summary>
+		public Switch(string name, string description)
+			: this(name, null, description, NoArguments, NotRequired)
+		{
+		}
+
+		/// <summary>
+		/// Constructor for specifying switch long name.
+		/// </summary>
+		public Switch(string name, string longName, string description)
+			: this(name, longName, description, NoArguments, NotRequired)
+		{
+		}
+
+		/// <summary>
+		/// Constructor for specifying whether switch required.
+		/// </summary>
+		public Switch(string name, string longName, string description,
+			bool isRequired)
+			: this(name, longName, description, NoArguments, isRequired)
+		{
+		}
+
+		/// <summary>
+		/// Constructor for specifying arguments expected to follow switch.
+		/// </summary>
+		public Switch(string name, string longName, string description,
+			bool hasArguments, bool isRequired)
+			: this(name, longName, description, UnknownNumberArguments,
+				isRequired)
+		{
+			m_hasArguments = hasArguments;
+		}
+
+		/// <summary>
+		/// Constructor for defining number of arguments expected to
+		/// follow switch.
+		/// </summary>
+		public Switch(string name, string longName, string description,
+			int numberArguments, bool isRequired)
+		{
+			m_argumentNames = new List<string>();
+			m_argumentValues = new List<string>();
+			m_description = description;
+			m_hasArguments = numberArguments > NoArguments;
+			m_isRequired = isRequired;
+			m_longName = longName == name ? null : longName;
+			m_name = name;
+			m_numberArguments = HasArguments ? numberArguments : NoArguments;
+		}
+
+		/// <summary>
+		/// Constructor for specifying arguments with given name expected to
+		/// follow switch.
+		/// </summary>
+		public Switch(string name, string longName, string description,
+			bool hasArguments, bool isRequired, string argumentName)
+			: this(name, longName, description, hasArguments, isRequired)
+		{
+			if (argumentName != null && HasArguments)
+				m_argumentNames.Add(argumentName);
+		}
+
+		/// <summary>
+		/// Constructor for defining argument name expected to follow switch.
+		/// </summary>
+		public Switch(string name, string longName, string description,
+			bool isRequired, string argumentName)
+			: this(name, longName, description, isRequired)
+		{
+			if (argumentName != null)
+			{
+				m_argumentNames.Add(argumentName);
+				m_hasArguments = true;
+				m_numberArguments = OneArgument;
+			}
+		}
+
+		/// <summary>
+		/// Constructor for defining argument names expected to follow switch.
+		/// </summary>
+		public Switch(string name, string longName, string description,
+			bool isRequired, string[] argumentNames)
+			: this(name, longName, description, isRequired)
+		{
+			if (argumentNames != null)
+			{
+				foreach (string argumentName in argumentNames)
+				{
+					if (argumentName != null)
+						m_argumentNames.Add(argumentName);
+				}
+
+				m_numberArguments = m_argumentNames.Count;
+				m_hasArguments = NumberArguments > NoArguments;
+			}
+		}
+
+		////////////////////////////////////////////////////////////////////////
+		// Public Methods
+
+		/// <summary>
+		/// Add name of argument expected to follow this switch.
+		/// </summary>
+		public void AddArgumentName(string name)
+		{
+			if (HasEnoughNames)
+				return;
+
+			if (name != null && name.Length > 0)
+				m_argumentNames.Add(name);
+		}
+
+		/// <summary>
+		/// Add value of argument expected to follow this switch.
+		/// </summary>
+		public void AddArgumentValue(string value)
+		{
+			if (HasEnoughValues)
+				return;
+
+			if (value != null && value.Length > 0)
+				m_argumentValues.Add(value);
+		}
+
+		/// <summary>
+		/// Get names of arguments following switch.
+		/// </summary>
+		public string[] GetArgumentNames()
+		{
+			return m_argumentNames.ToArray();
+		}
+
+		/// <summary>
+		/// Get values of arguments following switch.
+		/// </summary>
+		public string[] GetArgumentValues()
+		{
+			return m_argumentValues.ToArray();
+		}
+
+		/// <summary>
+		/// Get given switch name with long prefix.
+		/// </summary>
+		public static string GetLongPrefixedName(string name)
+		{
+			return GetPrefixedName(name, LongPrefix);
+		}
+
+		/// <summary>
+		/// Get switch name given command-line argument, which may include prefix.
+		/// </summary>
+		public static string GetName(string argument)
+		{
+			foreach (string prefix in Prefixes)
+			{
+				if (argument.StartsWith(prefix, StringComparison.Ordinal))
+					return argument.Substring(prefix.Length);
+			}
+
+			return argument;
+		}
+
+		/// <summary>
+		/// Get given switch name with prefix.
+		/// </summary>
+		public static string GetPrefixedName(string name)
+		{
+			return GetPrefixedName(name, Prefix);
+		}
+
+		/// <summary>
+		/// Determine if given command-line argument is valid switch.
+		/// </summary>
+		public static bool IsValid(string argument)
+		{
+			string switchName = GetName(argument);
+
+			if (switchName == argument)
+			{
+				// Command-line argument did not contain prefix.
+				return false;
+			}
+
+			// Valid if switch contains no other prefixes.
+			return !HasPrefix(switchName);
+		}
+
+		////////////////////////////////////////////////////////////////////////
+		// Methods
+
+		/// <summary>
+		/// Get given switch name with prefix.
+		/// </summary>
+		static string GetPrefixedName(string name, string prefix)
+		{
+			if (HasPrefix(name))
+				return name;
+
+			return prefix + name;
+		}
+
+		/// <summary>
+		/// Determine if given number of arguments enough for this switch.
+		/// </summary>
+		bool HasEnoughArguments(int numberArguments)
+		{
+			if (HasArguments)
+			{
+				if (NumberArguments == numberArguments)
+				{
+					return true;
+				}
+				else if (NumberArguments == UnknownNumberArguments &&
+					numberArguments > 0)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// Determine if given argument has prefix.
+		/// </summary>
+		static bool HasPrefix(string argument)
+		{
+			return argument != GetName(argument);
+		}
+
+		////////////////////////////////////////////////////////////////////////
+		// Properties
+
+		/// <summary>
+		/// Get switch description.
+		/// </summary>
+		public string Description
+		{
+			get { return m_description; }
+		}
+
+		/// <summary>
+		/// Determine if arguments expected to follow this switch.
+		/// </summary>
+		public bool HasArguments
+		{
+			get { return m_hasArguments; }
+		}
+
+		/// <summary>
+		/// Determine if switch has description.
+		/// </summary>
+		public bool HasDescription
+		{
+			get { return !string.IsNullOrEmpty(Description); }
+		}
+
+		/// <summary>
+		/// Determine if enough argument names added to this switch.
+		/// </summary>
+		public bool HasEnoughNames
+		{
+			get { return HasEnoughArguments(m_argumentNames.Count); }
+		}
+
+		/// <summary>
+		/// Determine if enough argument values added to this switch.
+		/// </summary>
+		public bool HasEnoughValues
+		{
+			get { return HasEnoughArguments(m_argumentValues.Count); }
+		}
+
+		/// <summary>
+		/// Determine if switch has long name.
+		/// </summary>
+		public bool HasLongName
+		{
+			get { return !string.IsNullOrEmpty(LongName); }
+		}
+
+		/// <summary>
+		/// Determine if optional switch.
+		/// </summary>
+		public bool IsOptional
+		{
+			get { return !IsRequired; }
+		}
+
+		/// <summary>
+		/// Determine if required switch.
+		/// </summary>
+		public bool IsRequired
+		{
+			get { return m_isRequired; }
+		}
+
+		/// <summary>
+		/// Get switch long name.
+		/// </summary>
+		public string LongName
+		{
+			get { return m_longName; }
+		}
+
+		/// <summary>
+		/// Get switch name.
+		/// </summary>
+		public string Name
+		{
+			get { return m_name; }
+		}
+
+		/// <summary>
+		/// Get number of arguments expected to follow this switch.
+		/// </summary>
+		public int NumberArguments
+		{
+			get { return m_numberArguments; }
+		}
+	}
+}
---
+++ trunk/Parse/ArgumentParser.cs
@@ -0,0 +1,247 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2010 Bernard Badjari
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Globalization;
+using CSharpCLI.Argument;
+
+namespace CSharpCLI.Parse
+{
+	/// <summary>
+	/// Parses command-line arguments.
+	/// </summary>
+	public class ArgumentParser
+	{
+		/// <summary>
+		/// Error messages.
+		/// </summary>
+		static class Messages
+		{
+			public const string RequiredSwitchMissing = "Required switch '{0}' not found.";
+			public const string SwitchAlreadyParsed = "Switch '{0}' already parsed.";
+			public const string SwitchMissingArgument = "Missing argument for switch '{0}'.";
+			public const string UndefinedSwitch = "Switch '{0}' undefined.";
+		}
+
+		/// <summary>
+		/// First argument number.
+		/// </summary>
+		const int FirstArgument = 1;
+
+		////////////////////////////////////////////////////////////////////////
+
+		/// <summary>
+		/// Command-line arguments to parse.
+		/// </summary>
+		string[] m_arguments;
+
+		/// <summary>
+		/// Switches parsed from command-line arguments, accessed by their name.
+		/// </summary>
+		SwitchCollection m_parsedSwitches;
+
+		/// <summary>
+		/// Expected switches to parse from command-line arguments.
+		/// </summary>
+		SwitchCollection m_switches;
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public ArgumentParser(string[] arguments, SwitchCollection switches)
+		{
+			m_arguments = arguments;
+			m_parsedSwitches = new SwitchCollection();
+			m_switches = switches;
+		}
+
+		////////////////////////////////////////////////////////////////////////
+		// Public Methods
+
+		/// <summary>
+		/// Determine if all switches with given names parsed.
+		/// </summary>
+		public bool AllParsed(params string[] names)
+		{
+			foreach (string name in names)
+			{
+				if (!IsParsed(name))
+					return false;
+			}
+
+			return true;
+		}
+
+		/// <summary>
+		/// Determine if any switches with given names parsed.
+		/// </summary>
+		public bool AnyParsed(params string[] names)
+		{
+			return !NoneParsed(names);
+		}
+
+		/// <summary>
+		/// Get first argument value from switch with given name.
+		/// 
+		/// If switch not parsed, return null.
+		/// </summary>
+		public string GetValue(string name)
+		{
+			return GetValue(name, FirstArgument);
+		}
+
+		/// <summary>
+		/// Get argument value from switch with given name and argument number.
+		/// 
+		/// If switch not parsed or invalid argument number, return null.
+		/// </summary>
+		public string GetValue(string name, int argumentNumber)
+		{
+			string[] values = GetValues(name);
+
+			string value = null;
+
+			if (values != null)
+			{
+				if (argumentNumber > 0 && argumentNumber <= values.Length)
+					value = values[argumentNumber - 1];
+			}
+
+			return value;
+		}
+
+		/// <summary>
+		/// Get argument values from switch with given name.
+		/// 
+		/// If switch not parsed, return null.
+		/// </summary>
+		public string[] GetValues(string name)
+		{
+			string[] values = null;
+
+			if (IsParsed(name))
+			{
+				Switch parsedSwitch = m_parsedSwitches[name];
+
+				values = parsedSwitch.GetArgumentValues();
+			}
+
+			return values;
+		}
+
+		/// <summary>
+		/// Determine if switch with given name parsed.
+		/// </summary>
+		public bool IsParsed(string name)
+		{
+			return m_parsedSwitches.HasSwitch(name);
+		}
+
+		/// <summary>
+		/// Determine if no switches with given names parsed.
+		/// </summary>
+		public bool NoneParsed(params string[] names)
+		{
+			foreach (string name in names)
+			{
+				if (IsParsed(name))
+					return false;
+			}
+
+			return true;
+		}
+
+		/// <summary>
+		/// Parse command-line arguments.
+		/// </summary>
+		public void Parse()
+		{
+			m_parsedSwitches.Clear();
+
+			for (int index = 0; index < m_arguments.Length; index++)
+			{
+				string argument = m_arguments[index];
+
+				if (Switch.IsValid(argument))
+				{
+					string switchName = Switch.GetName(argument);
+
+					if (!m_switches.HasSwitch(switchName))
+						ThrowParsingException(Messages.UndefinedSwitch, switchName);
+
+					if (IsParsed(switchName))
+						ThrowParsingException(Messages.SwitchAlreadyParsed, switchName);
+
+					Switch parsedSwitch = m_switches[switchName];
+
+					m_parsedSwitches.Add(parsedSwitch);
+
+					if (parsedSwitch.HasArguments)
+					{
+						for (index++; index < m_arguments.Length; index++)
+						{
+							string argumentValue = m_arguments[index];
+
+							if (Switch.IsValid(argumentValue))
+							{
+								// Parse this switch again.
+								index--;
+
+								break;
+							}
+
+							parsedSwitch.AddArgumentValue(argumentValue);
+						}
+
+						if (!parsedSwitch.HasEnoughValues)
+							ThrowParsingException(Messages.SwitchMissingArgument, switchName);
+					}
+				}
+			}
+
+			foreach (Switch currentSwitch in m_switches.Switches)
+			{
+				if (currentSwitch.IsRequired && !IsParsed(currentSwitch.Name))
+					ThrowParsingException(Messages.RequiredSwitchMissing, currentSwitch.Name);
+			}
+		}
+
+		////////////////////////////////////////////////////////////////////////
+		// Methods
+
+		/// <summary>
+		/// Throw ParsingException with given message and switch name.
+		/// </summary>
+		static void ThrowParsingException(string message, string name)
+		{
+			string formattedMessage = string.Format(CultureInfo.CurrentCulture,
+				message, name);
+
+			throw new ParsingException(formattedMessage);
+		}
+	}
+}
